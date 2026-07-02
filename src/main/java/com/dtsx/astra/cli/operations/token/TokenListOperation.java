bf27815f9a34428909f8218ba5b1cc16789afcbe
toptobes <kavinpg@gmail.com>
2026-07-02 18:19:18 -0500

    temp work on list-tokens

--- src/main/java/com/dtsx/astra/cli/operations/token/TokenListOperation.java
+++ src/main/java/com/dtsx/astra/cli/operations/token/TokenListOperation.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -29,21 +30,42 @@ public class TokenListOperation implements Operation<Stream<TokenInfo>> {
     public Stream<TokenInfo> execute() {
         val tokens = tokenGateway.findAll().toList();
 
+//        tokens.forEach((t) -> {
+//            try {
+//                t.getRoles().forEach(UUID::fromString);
+//            } catch (Exception _) {
+//                System.out.println(JsonUtils.formatJsonPretty(t));
+//            }
+//        });
+
         val roleMappings = roleGateway.findNames(
             tokens
                 .stream()
                 .flatMap((token) -> token.getRoles().stream())
-                .map(UUID::fromString)
+                .map((id) -> {
+                    try {
+                        return UUID.fromString(id);
+                    } catch (Exception e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
                 .collect(toSet())
         );
 
         return tokens.stream().map((token) -> {
             val roleIds = token.getRoles().stream()
-                .map(UUID::fromString)
+                .map((id) -> {
+                    try {
+                        return UUID.fromString(id); // ugly duplication and null usage but whatever it's fine for now
+                    } catch (Exception e) {
+                        return null;
+                    }
+                })
                 .toList();
 
             val roleNames = roleIds.stream()
-                .map((roleId) -> roleMappings.get(roleId).orElse(roleId.toString()))
+                .map((roleId) -> roleMappings.get(roleId).orElse((roleId != null) ? roleId.toString() : null))
                 .toList();
 
             return new TokenInfo(roleNames, roleIds, token);
