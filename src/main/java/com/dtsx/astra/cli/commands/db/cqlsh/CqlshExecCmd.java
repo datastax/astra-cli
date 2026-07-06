e63f30f29a0709ae73c59a46af041dcdd53fa7f9
toptobes <kavinpg@gmail.com>
2026-07-05 23:34:12 -0500

    remove positional param cql from cqlsh exec

--- src/main/java/com/dtsx/astra/cli/commands/db/cqlsh/CqlshExecCmd.java
+++ src/main/java/com/dtsx/astra/cli/commands/db/cqlsh/CqlshExecCmd.java
@@ -22,15 +22,6 @@ import java.util.Optional;
     command = "${cli.name} db cqlsh exec my_db -f script.cql"
 )
 public class CqlshExecCmd extends CqlshStartImpl {
-    @Parameters(
-        index = "1",
-        description = "Execute the statement and quit",
-        paramLabel = "STATEMENT",
-        arity = "0..1",
-        hidden = true
-    )
-    public Optional<String> $legacyStatement; // this was a mistake because it doesn't work well w/ optional first param
-
     @ArgGroup
     public RawExecSource $execSource;
 
@@ -59,26 +50,13 @@ public class CqlshExecCmd extends CqlshStartImpl {
 
     @Override
     protected Optional<ExecSource> execSource() {
-        if ($legacyStatement.isPresent()) {
-            ctx.log().warn("Providing the CQL statement as a positional parameter is @!deprecated!@ and will be removed in future versions.");
-            ctx.log().warn("Please use the @'!${cli.name} db cqlsh exec -e \"CQL STATEMENT\"!@ syntax instead.");
-        }
-
         val source = Objects.requireNonNullElseGet($execSource, RawExecSource::new);
 
-        if ($legacyStatement.isPresent() && source.$statement.isPresent()) {
-            throw new ParameterException(spec.commandLine(), "Cannot specify the CQL statement both as a positional parameter and with the -e/--execute option; please use the -e flag from now on, as positional statements are deprecated.");
-        }
-
-        val statement = (source.$statement.isPresent())
-            ? $execSource.$statement
-            : $legacyStatement;
-
-        if (statement.isPresent() && source.$file.isPresent()) {
+        if ($execSource.$statement.isPresent() && source.$file.isPresent()) {
             throw new ParameterException(spec.commandLine(), "Cannot specify both a statement and a file to execute; please choose just one.");
         }
 
-        val execSource = statement.filter(s -> !s.trim().equals("-"))
+        val execSource = $execSource.$statement.filter(s -> !s.trim().equals("-"))
             .<ExecSource>map(ExecSource.Statement::new)
             .or(() -> source.$file.map(ExecSource.CqlFile::new))
             .orElseGet(ExecSource.Stdin::new);
