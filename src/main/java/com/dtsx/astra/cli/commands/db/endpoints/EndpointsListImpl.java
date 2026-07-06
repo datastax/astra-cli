fee524154030c5143eecae3268eb8da373311cc2
toptobes <kavinpg@gmail.com>
2026-07-06 02:22:39 -0500

    make astra db endpoints an alias for astra db endpoints list

---
+++ src/main/java/com/dtsx/astra/cli/commands/db/endpoints/EndpointsListImpl.java
@@ -0,0 +1,47 @@
+package com.dtsx.astra.cli.commands.db.endpoints;
+
+import com.dtsx.astra.cli.commands.db.AbstractPromptForDbCmd;
+import com.dtsx.astra.cli.core.CliConstants.$Regions;
+import com.dtsx.astra.cli.core.models.RegionName;
+import com.dtsx.astra.cli.core.output.formats.OutputAll;
+import com.dtsx.astra.cli.core.output.table.ShellTable;
+import com.dtsx.astra.cli.operations.Operation;
+import com.dtsx.astra.cli.operations.db.endpoints.EndpointGetOperation;
+import com.dtsx.astra.cli.operations.db.endpoints.EndpointGetOperation.EndpointGetRequest;
+import com.dtsx.astra.cli.operations.db.endpoints.EndpointGetOperation.EndpointGetResponse;
+import lombok.val;
+import picocli.CommandLine.Option;
+
+import java.util.LinkedHashMap;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public abstract class EndpointsListImpl extends AbstractPromptForDbCmd<EndpointGetResponse> {
+    @Option(
+        names = { $Regions.LONG, $Regions.SHORT },
+        description = "The region to use",
+        paramLabel = $Regions.LABEL
+    )
+    protected Optional<RegionName> region;
+
+    @Override
+    protected OutputAll execute(Supplier<EndpointGetResponse> result) {
+        val endpoints = new LinkedHashMap<String, Object>();
+
+        for (val endpoint : Endpoint.values()) {
+            endpoints.put(endpoint.displayName(), endpoint.mkUrl(result.get(), profile().env()));
+        }
+
+        return ShellTable.forAttributes(endpoints, "Endpoint", "URL");
+    }
+
+    @Override
+    protected final Operation<EndpointGetResponse> mkOperation() {
+        return new EndpointGetOperation(dbGateway, new EndpointGetRequest($dbRef, region, profile().env()));
+    }
+
+    @Override
+    protected final String dbRefPrompt() {
+        return "Select the database to get the endpoint for";
+    }
+}
