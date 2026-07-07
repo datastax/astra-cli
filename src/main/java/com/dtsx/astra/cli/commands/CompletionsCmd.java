844a704a1499f5533dc5021e1eb08646da6d275c
toptobes <kavinpg@gmail.com>
2026-07-07 01:09:05 -0500

    fix a minor todo

--- src/main/java/com/dtsx/astra/cli/commands/CompletionsCmd.java
+++ src/main/java/com/dtsx/astra/cli/commands/CompletionsCmd.java
@@ -75,9 +75,8 @@ public class CompletionsCmd implements Runnable {
 
         val props = CliPropertiesImpl.mkAndLoadSysProps(new CliEnvironmentImpl());
 
-        // TODO should check for -p as well
         sb.append("""
-          get_profile(){ for ((i=0;i<${#COMP_WORDS[@]};i++));do [[ ${COMP_WORDS[i]} == --profile ]]&&((i+1<${#COMP_WORDS[@]}))&&echo ${COMP_WORDS[i+1]}&&return;done; echo default;};
+          get_profile(){ for ((i=0;i<${#COMP_WORDS[@]};i++));do [[ "${COMP_WORDS[i]}" == --profile || "${COMP_WORDS[i]}" == -p ]]&&((i+1<${#COMP_WORDS[@]}))&&echo ${COMP_WORDS[i+1]}&&return;done; echo default;};
         """).append(NL);
 
         sb.append("""
